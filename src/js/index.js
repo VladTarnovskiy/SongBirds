50ff1777856f78c5d762ec722d9ae025e2b713c6
vladtarnovskiy <vladtarnovski989@gmail.com>
2023-09-08 15:44:40 +0300

    feat: remove comment code

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,5 @@
-import '../html/index.html'
-import '../scss/index.scss'
-import './moduls/birds'
-import './components/game'
-import '../js/components/loader'
-
-
-// import '../audio/false-click.mp3'
-// import '../audio/true-click.mp3'
+import "../html/index.html";
+import "../scss/index.scss";
+import "./moduls/birds";
+import "./components/game";
+import "../js/components/loader";
